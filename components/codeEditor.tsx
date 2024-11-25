01ea96ce817303f520ab1c2b7ccfe5c3477c8b8a
axl0926 <song22092000@gmail.com>
2024-11-25 15:39:43 +0900

    Feat:CodeEditor 컴포넌트 추가

--- components/codeEditor.tsx
+++ components/codeEditor.tsx
@@ -1,8 +1,19 @@
 "use client";
 import { Editor } from "@monaco-editor/react";
+import { Dispatch, SetStateAction } from "react";
+type Props = {
+  code: string;
+  language: string;
+  setCode: Dispatch<SetStateAction<string>>;
+};
 
-const CodeEditor = () => {
-  return <Editor className="h-full w-full" theme="vs-dark" language="javascript" />;
+const CodeEditor = ({ code, setCode, language }: Props) => {
+  const changeEventHandler = (value: string | undefined) => {
+    setCode(value || "");
+  };
+  return (
+    <Editor className="h-full w-full" theme="vs-dark" language={language} value={code} onChange={changeEventHandler} />
+  );
 };
 
 export default CodeEditor;
