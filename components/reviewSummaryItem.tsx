88eba955600897978b9ec75c15e2cb4113c88a00
juriurj <bumakker@gmail.com>
2024-11-25 14:47:46 +0900

    Feat: Avatar component 추가

--- components/reviewSummaryItem.tsx
+++ components/reviewSummaryItem.tsx
@@ -1,18 +1,29 @@
 "use client";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 type Props = {
   title: string;
   content: string;
+  created_at: string;
+  user_img?: string;
 };
 
-const ReviewSummaryItem = ({ title, content }: Props) => {
+const ReviewSummaryItem = ({ title, content, created_at, user_img = "https://github.com/shadcn.png" }: Props) => {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>{title}</CardTitle>
+        <CardTitle className="flex items-center">
+          <Avatar>
+            <AvatarImage src={user_img} />
+          </Avatar>
+          <span className="px-4 text-2xl">{title}</span>
+        </CardTitle>
       </CardHeader>
-      <CardContent>{content}</CardContent>
+      <CardContent>
+        <div>{content}</div>
+        <div>{created_at}</div>
+      </CardContent>
     </Card>
   );
 };
