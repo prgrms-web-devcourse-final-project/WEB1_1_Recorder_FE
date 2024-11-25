497e250048b42fbdd299de0b4f27c0fb18b06896
axl0926 <song22092000@gmail.com>
2024-11-25 17:15:39 +0900

    Feat:CodeEditor components readonly속성 추가

--- components/codeEditor.tsx
+++ components/codeEditor.tsx
@@ -5,14 +5,22 @@ type Props = {
   code: string;
   language: string;
   setCode: Dispatch<SetStateAction<string>>;
+  readonly?: boolean;
 };
 
-const CodeEditor = ({ code, setCode, language }: Props) => {
+const CodeEditor = ({ code, setCode, language, readonly = false }: Props) => {
   const changeEventHandler = (value: string | undefined) => {
     setCode(value || "");
   };
   return (
-    <Editor className="h-full w-full" theme="vs-dark" language={language} value={code} onChange={changeEventHandler} />
+    <Editor
+      className="h-full w-full"
+      theme="vs-dark"
+      language={language}
+      value={code}
+      onChange={changeEventHandler}
+      options={{ readOnly: readonly }}
+    />
   );
 };
 
