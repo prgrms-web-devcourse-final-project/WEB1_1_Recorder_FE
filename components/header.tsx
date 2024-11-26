1cd847198cd5f75419f6e4a315b2d66c6293aaea
이주환 <wnghks145@gmail.com>
2024-11-26 17:09:49 +0900

    Design: 헤더 전체 디자인 수정

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,17 @@
 import Image from 'next/image';
+import HeaderMain from '../components/header/headerMain';
+import HeaderFooter from '../components/header/headerFooter';
 
 type Props = object;
 
 const Header = ({ }: Props) => {
   return (
-<div className="header">
-    <Image src="/img/logo.png" alt="logo" width={150} height={50}/>
+    <div className="flex items-center justify-between p-[40px] border-b-2 border-[#1A202C]">
+      <div className="flex items-center gap-[60px]">
+      <Image src="/img/logo.png" alt="logo" width={150} height={50}/>
+      <HeaderMain />
+      </div>
+      <HeaderFooter />
     </div>
   );
 };
