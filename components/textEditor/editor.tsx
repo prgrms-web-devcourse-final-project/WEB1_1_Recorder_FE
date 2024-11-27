35aef6e582d383588e8ebe69171efd166f491da4
axl0926 <song22092000@gmail.com>
2024-11-27 15:44:38 +0900

    Feat:TextEditor componets 추가

---
+++ components/textEditor/editor.tsx
@@ -0,0 +1,81 @@
+"use client";
+// InitializedMDXEditor.tsx
+import type { ForwardedRef } from "react";
+import {
+  headingsPlugin,
+  listsPlugin,
+  quotePlugin,
+  thematicBreakPlugin,
+  markdownShortcutPlugin,
+  MDXEditor,
+  type MDXEditorMethods,
+  type MDXEditorProps,
+  toolbarPlugin,
+  UndoRedo,
+  BoldItalicUnderlineToggles,
+  CodeToggle,
+  ListsToggle,
+  CreateLink,
+  linkDialogPlugin,
+  imagePlugin,
+  InsertImage,
+  InsertThematicBreak,
+  codeMirrorPlugin,
+  codeBlockPlugin,
+  InsertCodeBlock,
+  InsertAdmonition,
+  directivesPlugin,
+  AdmonitionDirectiveDescriptor
+} from "@mdxeditor/editor";
+
+type Props = { editorRef: ForwardedRef<MDXEditorMethods> | null } & MDXEditorProps;
+
+const Editor = ({ editorRef, ...props }: Props) => {
+  return (
+    <MDXEditor
+      plugins={[
+        toolbarPlugin({
+          toolbarContents: () => (
+            <>
+              <UndoRedo />
+              <BoldItalicUnderlineToggles />
+              <CodeToggle />
+              <ListsToggle />
+              <CreateLink />
+              <InsertImage />
+              <InsertThematicBreak />
+              <InsertCodeBlock />
+              <InsertAdmonition />
+            </>
+          )
+        }),
+        imagePlugin({
+          imageAutocompleteSuggestions: ["https://via.placeholder.com/150", "https://via.placeholder.com/150"],
+          imageUploadHandler: async () => Promise.resolve("https://picsum.photos/200/300")
+        }),
+        linkDialogPlugin(),
+        headingsPlugin(),
+        listsPlugin(),
+        quotePlugin(),
+        thematicBreakPlugin(),
+        markdownShortcutPlugin(),
+        codeBlockPlugin({ defaultCodeBlockLanguage: "" }),
+        codeMirrorPlugin({
+          codeBlockLanguages: {
+            js: "JavaScript",
+            css: "CSS",
+            txt: "Plain Text",
+            tsx: "TypeScript",
+            "": "Unspecified"
+          }
+        }),
+        directivesPlugin({
+          directiveDescriptors: [AdmonitionDirectiveDescriptor]
+        })
+      ]}
+      {...props}
+      ref={editorRef}
+    />
+  );
+};
+export default Editor;
