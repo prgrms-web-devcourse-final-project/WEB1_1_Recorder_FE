d95b8367378822b36187099974200f8625590f2b
이주환 <wnghks145@gmail.com>
2024-11-27 14:30:03 +0900

    Refactor: page.tsx 구조 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 import Footer from '../components/footer';
 
-const Home = () => {
+const Home = ({}) => {
   return (
-    // Header도 함께 추가 예정
     <div className="flex flex-col min-h-screen">
       <main className="flex-grow">
       </main>
