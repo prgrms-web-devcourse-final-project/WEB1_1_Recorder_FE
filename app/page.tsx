531cab2651293fb43f7c973cbaf08c7dffdc73a9
이주환 <wnghks145@gmail.com>
2024-11-27 14:27:15 +0900

    Comment: 헤더 추가 주석

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Footer from '../components/footer';
 
 const Home = () => {
   return (
+    // Header도 함께 추가 예정
     <div className="flex flex-col min-h-screen">
       <main className="flex-grow">
       </main>
