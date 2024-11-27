c6f68dc9aa33b25ab94b82baead169596186f5d1
juriurj <bumakker@gmail.com>
2024-11-27 17:47:40 +0900

    Feat: reviewsPage 생성

---
+++ app/reviews/page.tsx
@@ -0,0 +1,13 @@
+import SearchBox from "@/components/questions/searchBox";
+
+type Props = {};
+
+const Reviews = ({}: Props) => {
+  return (
+    <main className="m-auto my-10 max-w px-4">
+      <SearchBox />
+    </main>
+  );
+};
+
+export default Reviews;
