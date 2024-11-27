ad905cfcd3bd179417c3f79330cf6195a6a3392d
이주환 <wnghks145@gmail.com>
2024-11-27 14:53:27 +0900

    Comment: page.tsx 주석 제거

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,8 @@ import Footer from '../components/footer';
 
 const Home = ({}) => {
   return (
-    <div className="flex flex-col min-h-screen">
-      <main className="flex-grow">
-      </main>
-      <Footer />
+    <div className="">
+      
     </div>
   );
 };
