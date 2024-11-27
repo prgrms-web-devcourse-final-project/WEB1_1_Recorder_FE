f6cc440caebcbbe188a21cf77fd25deb7fb2294c
이주환 <wnghks145@gmail.com>
2024-11-27 15:51:28 +0900

    Design : footer 추가 후 배치 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,29 +2,33 @@ import type { Metadata } from "next";
 import "./globals.css";
 import localFont from "next/font/local";
 import HeaderMember from "@/components/headerMember";
+import Footer from "@/components/footer";
 
 const pretendard = localFont({
   src: "./fonts/PretendardVariable.woff2",
   display: "swap",
   weight: "45 920",
-  variable: "--font-pretendard"
+  variable: "--font-pretendard",
 });
 
 export const metadata: Metadata = {
   title: "RevUP",
-  description: "RevUP"
+  description: "RevUP",
 };
 
 export default function RootLayout({
-  children
+  children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className={`${pretendard.variable} font-pretendard`}>
+      <body
+        className={`${pretendard.variable} font-pretendard flex flex-col min-h-screen`}
+      >
         <HeaderMember />
-        {children}
+        <main className="flex-grow">{children}</main>
+        <Footer />
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Footer from '../components/footer';
 import MentorSummaryItem from "@/components/mentorSummaryItem";
 import ReviewSummaryList from "@/components/reviewSummaryList";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
