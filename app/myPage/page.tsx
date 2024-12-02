ae8aaad8500ae6fa5d304a507f168652fa081847
이주환 <wnghks145@gmail.com>
2024-12-02 15:18:07 +0900

    Review : 빈 속성 제거

--- app/myPage/page.tsx
+++ app/myPage/page.tsx
@@ -1,21 +1,23 @@
 "use client";
 import PageHeader from "@/components/pageHeader";
-import MyInfoProfile from "@/components/mypage/myInfoProfile"
+import MyInfoProfile from "@/components/mypage/myInfoProfile";
 import MyPageTop from "@/components/mypage/myPageTop";
-import MyChallengeTop from "@/components/mypage/myChallengeTop"
+import MyChallengeTop from "@/components/mypage/myChallengeTop";
 import MyChallenge from "@/components/mypage/myChallenge";
+import MyStackTop from "@/components/mypage/myStackTop";
+import MyStack from "@/components/mypage/myStack";
 
 const MyPage = () => {
   return (
-    <div className="m-auto my-10 max-w px-4 lg:px-20 h-screen">
+    <div className="m-auto my-10 h-full max-w px-4 lg:px-20">
       <PageHeader title="마이페이지" />
       <div className="mt-10">
         <MyPageTop />
-        <div className="">
-          <MyInfoProfile />
-        </div>
+        <MyInfoProfile />
         <MyChallengeTop />
         <MyChallenge />
+        <MyStackTop />
+        <MyStack />
       </div>
     </div>
   );
