d6826642d15ef8258d6fe026a3d4c01dc2fcc8c1
이주환 <wnghks145@gmail.com>
2024-12-02 12:10:45 +0900

    Refactor : 마이페이지 탑 컴포넌트 분리

---
+++ components/mypage/myPageTop.tsx
@@ -0,0 +1,16 @@
+type Props = object;
+
+const MyPageTop = ({}: Props) => {
+  return (
+    <div className="flex gap-6 border-b-2 mb-5">
+      <p className="text-24pt cursor-pointer border-b-2 border-transparent hover:border-[#2D3648]">
+        나의 정보
+      </p>
+      <p className="text-24pt cursor-pointer border-b-2 border-transparent hover:border-[#2D3648]">
+        나의 활동
+      </p>
+    </div>
+  );
+};
+
+export default MyPageTop;
