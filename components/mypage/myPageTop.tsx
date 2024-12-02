43eab08d4e3f80bcb630767ac4e6ef3357fa17b7
이주환 <wnghks145@gmail.com>
2024-12-02 14:43:21 +0900

    Design : 마이페이지 상단 css 수정

--- components/mypage/myPageTop.tsx
+++ components/mypage/myPageTop.tsx
@@ -2,7 +2,7 @@ type Props = object;
 
 const MyPageTop = ({}: Props) => {
   return (
-    <div className="flex gap-6 border-b-2 mb-5">
+    <div className="flex gap-6 border-b-2 mb-10">
       <p className="text-24pt cursor-pointer border-b-2 border-transparent hover:border-[#2D3648]">
         나의 정보
       </p>
