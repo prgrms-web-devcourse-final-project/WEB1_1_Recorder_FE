52d97236a44386bf356f4d6461f310b6e1853c56
이주환 <wnghks145@gmail.com>
2024-12-02 12:52:03 +0900

    Refactor : 나의 정보 레이아웃 구현

--- components/mypage/myInfoProfile.tsx
+++ components/mypage/myInfoProfile.tsx
@@ -1,20 +1,38 @@
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 type Props = object;
 
-const myInfoProfile = ({ }: Props) => {
+const MyInfoProfile = ({}: Props) => {
+  const router = useRouter();
   return (
-<div className="flex-col items-center bg-[#E8E8E8] rounded-[10px]">
-    <Image className="bg-white border-none rounded-full" src="" alt="profile" width={150} height={150}/>
-    <div className="flex flex-col gap-2">
-        <p>name</p>
-        <p>test@email.com</p>
-        <div className="flex items-center">
+    <div className="flex flex-col bg-[#E8E8E8] rounded-[10px] p-6">
+      <div className="flex flex-col items-center justify-center">
+        <Image
+          className="bg-white border-none rounded-full"
+          src=""
+          alt="profile"
+          width={150}
+          height={150}
+        />
+        <div className="flex flex-col gap-2 items-center mt-4">
+          <div className="flex items-center gap-2">
+            <Image src="/img/reward.png" alt="reward" width={30} height={30} />
+            <p>name</p>
+          </div>
+          <p>test@email.com</p>
+          <div className="flex items-center gap-2">
             <p>계정 연동</p>
-            <Image src="/img/logo.svg" alt="logo" width={30} height={30}/>
+            <Image src="/img/github_grayimg.png" alt="social" width={40} height={30} />
+          </div>
         </div>
+      </div>
+      <div className="flex justify-end mt-4 mr-12">
+        <button className="bg-[#2D3648] w-[175px] p-[10px] text-white rounded-sm" onClick={() => router.push("/myPage/editProfile")}>
+          회원정보수정
+        </button>
+      </div>
     </div>
-  </div>
   );
 };
 
-export default myInfoProfile;
+export default MyInfoProfile;
