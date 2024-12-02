d8075626829e48b8d8baae875da7dfeedabcced3
이주환 <wnghks145@gmail.com>
2024-12-02 21:58:55 +0900

    Refactor : 마이페이지 디렉토리 분리

--- components/mypage/myInfo/myInfoProfile.tsx
+++ components/mypage/myInfo/myInfoProfile.tsx
@@ -1,23 +1,24 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
 type Props = object;
 
 const MyInfoProfile = ({}: Props) => {
   const router = useRouter();
   return (
-    <div className="flex flex-col bg-[#E8E8E8] rounded-[10px] p-6">
+    <div className="flex flex-col bg-secondary rounded-[10px] p-6">
       <div className="flex flex-col items-center justify-center">
         <Image
           className="bg-white border-none rounded-full"
-          src="/img/kakao_img.png"
-          alt="profile"
+          src="img/svg/logo.svg"
+          alt=""
           width={150}
           height={150}
         />
         <div className="flex flex-col gap-2 items-center mt-4">
           <div className="flex items-center gap-2">
             <Image src="/img/reward.png" alt="reward" width={30} height={30} />
-            <p>name</p>
+            <p>test1</p>
           </div>
           <p>test@email.com</p>
           <div className="flex items-center gap-2">
@@ -27,9 +28,9 @@ const MyInfoProfile = ({}: Props) => {
         </div>
       </div>
       <div className="flex justify-end mt-4 mr-12">
-        <button className="bg-[#2D3648] w-[175px] p-[10px] text-white rounded-sm" onClick={() => router.push("/myPage/editProfile")}>
+        <Button size="edit" className="bg-btnColor text-white" onClick={() => router.push("/myPage/editProfile")}>
           회원정보수정
-        </button>
+        </Button>
       </div>
     </div>
   );
