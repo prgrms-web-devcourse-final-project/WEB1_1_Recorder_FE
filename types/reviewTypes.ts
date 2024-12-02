0cf948f1f6491c17073aaa858cdbc2dc1b3927a3
axl0926 <song22092000@gmail.com>
2024-12-02 12:54:08 +0900

    Feat : 더미 데이터 및 타입 추가

--- types/reviewTypes.ts
+++ types/reviewTypes.ts
@@ -11,3 +11,39 @@ export type Question = {
   is_deleted: boolean;
   is_anomyous: boolean;
 };
+
+export type Code = {
+  name: string;
+  content: string;
+};
+
+export type Answer = {
+  id: number;
+  title: string;
+  writer: string;
+  content: string;
+  createdAt: string;
+  isAccept: boolean;
+  goodCount: number;
+  badCount: number;
+  codes: Code;
+};
+
+export type Result = {
+  id: number;
+  writer: string;
+  title: string;
+  createdAt: string;
+  answerCount: number;
+  readCount: number;
+  githubLink: string;
+  stacks: string[];
+  codes: Code[];
+  content: string;
+  answers: Answer[];
+};
+
+export type TReviewDetail = {
+  message: string;
+  result: Result;
+};
