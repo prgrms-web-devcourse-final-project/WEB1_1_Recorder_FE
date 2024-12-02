4296cca4f3f3851758efc2a88d03d0267a1b4894
이주환 <wnghks145@gmail.com>
2024-12-02 16:45:32 +0900

    Refactor : 사용자 정의 색상 적용

--- components/mypage/myChallenge.tsx
+++ components/mypage/myChallenge.tsx
@@ -4,11 +4,11 @@ import { Progress } from "@/components/ui/progress"
 const myChallenge = ({}: Props) => {
   return (
     <div className="mt-10">
-      <div className="flex flex-col rounded-[10px] bg-background px-[120px] py-[70px]">
+      <div className="flex flex-col rounded-[10px] px-[120px] py-[70px]">
         <div className="flex flex-col items-center justify-center">
             <div className="flex gap-10">
                 <p className="text-2xl">달성한 업적 3개</p>
-                <div className="border-r-2 border-primary"></div>
+                <div className="border-r-2"></div>
                 <p className="text-2xl">총 업적 10개</p>
             </div>
             <Progress value={33} className="mt-5"/>
