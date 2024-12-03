d19c9451e041e1cbd83842b9b182f60427a887a3
이주환 <wnghks145@gmail.com>
2024-12-03 20:43:59 +0900

    Refactor : 나의 답변, 채택된 답변 컴포넌트 분리

---
+++ components/mypage/myActivity/AcceptedAnswer.tsx
@@ -0,0 +1,13 @@
+import PageHeader from "@/components/pageHeader";
+
+type Props = object
+
+const AcceptedAnswer = ({ }: Props) => {
+  return (
+<div className="">
+    <PageHeader title="채택된 답변"/>
+    </div>
+  );
+};
+
+export default AcceptedAnswer;
