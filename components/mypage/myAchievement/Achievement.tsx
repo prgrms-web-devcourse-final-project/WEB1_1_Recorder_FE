a4130ccd0c04dca8089e779e99645eafaf9bb41b
이주환 <wnghks145@gmail.com>
2024-12-04 16:16:05 +0900

    Refactor : 업적 현황 컴포넌트 분리

--- components/mypage/myAchievement/Achievement.tsx
+++ components/mypage/myAchievement/Achievement.tsx
@@ -1,13 +1,13 @@
+import Image from "next/image"
 import PageHeader from "@/components/pageHeader";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Progress } from "@/components/ui/progress";
 import ChangeModal from "@/components/mypage/myInfo/changeModal";
-import Image from "next/image";
+import AchievementStatus from "@/components/mypage/myAchievement/AchievementStatus";
 type Props = object;
 
 const myAchievement = ({}: Props) => {
   return (
-    <div className="m-auto my-10 h-screen max-w px-4 lg:px-20">
+    <div className="m-auto my-10 h-full max-w px-4 lg:px-20">
       <PageHeader title="나의 업적" />
       <div className="mt-10 flex flex-col rounded-[10px] bg-secondary p-6">
         <div className="flex items-center justify-start gap-[50px] px-[70px] py-20">
@@ -17,8 +17,8 @@ const myAchievement = ({}: Props) => {
           <div className="mt-4 flex flex-col gap-2">
             <div className="flex gap-5">
               <div className="flex items-center gap-2">
-                <Image src="/img/bronze.png" alt="" width={20} height={20}/>
-                <p>채택 답변수</p>
+                <Image src="/img/silver.png" alt="" width={20} height={20} />
+                <p>채택률</p>
               </div>
               <ChangeModal />
             </div>
@@ -27,23 +27,7 @@ const myAchievement = ({}: Props) => {
           </div>
         </div>
       </div>
-
-      <div className="mt-8 flex items-center justify-between gap-6 border-b-2 border-btnColor">
-        <p className="pb-3 text-[32px] font-bold">업적 현황</p>
-      </div>
-
-      <div className="mt-10">
-        <div className="flex flex-col rounded-[10px] px-[120px] py-[70px]">
-          <div className="flex flex-col items-center justify-center">
-            <div className="flex gap-10">
-              <p className="text-2xl">달성한 업적 1개</p>
-              <div className="border-r-2"></div>
-              <p className="text-2xl">총 업적 5개</p>
-            </div>
-            <Progress value={20} className="mt-5" />
-          </div>
-        </div>
-      </div>
+      <AchievementStatus/>
     </div>
   );
 };
