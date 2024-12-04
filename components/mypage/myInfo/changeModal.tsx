fd1726140fb0d7da09d9162bda440a14aaa66ee7
이주환 <wnghks145@gmail.com>
2024-12-04 16:17:21 +0900

    Refactor : 업적 변경 간소한 모달창 구현

--- components/mypage/myInfo/changeModal.tsx
+++ components/mypage/myInfo/changeModal.tsx
@@ -6,16 +6,19 @@ const changeModal = ({}: Props) => {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button>업적 변경</Button>
+        <Button size="change" className="bg-btnColor text-white">
+          업적 변경
+        </Button>
+        
       </DialogTrigger>
       <DialogContent className="w-full h-5/6 overflow-auto">
         <DialogHeader>
           <DialogTitle>업적 변경하기</DialogTitle>
         </DialogHeader>
         <div className="flex justify-center gap-5">
-            <Button className="text-white">채택된 답변 수</Button>
-            <Button className="text-white">누적 추천 수</Button>
-            <Button className="text-white">좋아요 수</Button>
+            <Button className="text-white">채택률</Button>
+            <Button className="text-white">N일 연속 답변</Button>
+            <Button className="text-white">채택 답변 개수</Button>
         </div>
       </DialogContent>
     </Dialog>
