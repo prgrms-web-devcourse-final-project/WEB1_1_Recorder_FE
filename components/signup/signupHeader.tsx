880162f321e3194f995aa9a83df4442f4e2472e4
이주환 <wnghks145@gmail.com>
2024-12-06 14:50:50 +0900

    Feat : 프로필 이미지 업로드

--- components/signup/signupHeader.tsx
+++ components/signup/signupHeader.tsx
@@ -1,26 +1,32 @@
 import Image from "next/image";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Input } from "@/components/ui/input";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 type Props = object;
 
-const signupHeader = ({}: Props) => {
+const SignupHeader = ({}: Props) => {
   const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const [avatarUrl, setAvatarUrl] = useState<string>("https://github.com/shadcn.png");
 
   const handleImageClick = () => {
-    if (fileInputRef.current) {
-      fileInputRef.current.click(); // 파일 업로드 창 열기
+    fileInputRef.current?.click();
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setAvatarUrl(imageUrl);
     }
   };
 
   return (
     <div className="flex flex-col items-center gap-[30px]">
-      {/* <Image src="/svg/logo.svg" alt="logo" width={300} height={100} /> */}
       <p className="text-[48px] font-extrabold">Sign Up</p>
       <div className="relative flex items-start">
         <Avatar className="h-40 w-40">
-          <AvatarImage src="https://github.com/shadcn.png" className="h-40 w-40 rounded-full border-none bg-white" />
+          <AvatarImage src={avatarUrl} className="h-40 w-40 rounded-full border-none bg-white" />
         </Avatar>
         <Image
           src="/img/upload.png"
@@ -30,10 +36,17 @@ const signupHeader = ({}: Props) => {
           className="absolute bottom-[-5%] right-[-10%] cursor-pointer"
           onClick={handleImageClick}
         />
-        <Input id="picture" type="file" className="hidden" ref={fileInputRef} />
+        <Input
+          id="picture"
+          type="file"
+          accept="image/*"
+          className="hidden"
+          ref={fileInputRef}
+          onChange={handleFileChange}
+        />
       </div>
     </div>
   );
 };
 
-export default signupHeader;
+export default SignupHeader;
