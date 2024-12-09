56537bb6f0ad002e05ddfedfe5145062db2c021d
juriurj <bumakker@gmail.com>
2024-12-09 11:05:22 +0900

    Feat: 채팅 보내기 기능 구현

--- components/chat/chatDetail.tsx
+++ components/chat/chatDetail.tsx
@@ -11,14 +11,17 @@ type Props = {
   className: string;
 };
 
+type Message = { content: string; authorization: string; type: "CHAT" };
+
 const ChatDetail = ({ id, className }: Props) => {
-  const [messages, setMessages] = useState<string[]>([]);
+  const [messages, setMessages] = useState<Message[]>([]);
   const [client, setClient] = useState<Client | null>(null);
+  const [input, setInput] = useState("");
+  const [sendInput, setSendInput] = useState("");
 
   useEffect(() => {
     const token = Cookies.get("accessToken");
     const headers = { Authorization: token };
-
     const socket = new SockJS(`${process.env.NEXT_PUBLIC_API_URL}/ws/chat`);
     const stompClient = Stomp.over(socket);
 
@@ -38,7 +41,6 @@ const ChatDetail = ({ id, className }: Props) => {
     );
 
     setClient(stompClient);
-
     return () => stompClient.deactivate();
   }, []);
 
@@ -51,17 +53,25 @@ const ChatDetail = ({ id, className }: Props) => {
     }
   };
 
+  useEffect(() => {
+    const token = Cookies.get("accessToken");
+
+    if (token) {
+      const m = { content: sendInput, authorization: `Bearer ${token}`, type: "CHAT" };
+      sendMessage(JSON.stringify(m));
+    }
+  }, [sendInput]);
+
   return (
     <div className={`relative ${className}`}>
-      {/* <div className="flex h-full flex-col-reverse items-end pb-28">
-        {messageList.map((message, i) => {
-          return <ChatBubble key={i}>{message}</ChatBubble>;
+      <div className="flex h-full flex-col items-end justify-end pb-28">
+        {messages.map((message, i) => {
+          return <ChatBubble key={i}>{message.content}</ChatBubble>;
         })}
       </div>
       <div className="absolute bottom-4 w-full pr-8">
-        <ChatInput className="h-24" state={messageList} setState={setMessageList} ref={messageRef} />
+        <ChatInput className="h-24" state={input} setState={setInput} setSendInput={setSendInput} />
       </div>
-      <button onClick={() => sendMessage("안녕")}>보내기</button> */}
     </div>
   );
 };
--- components/chat/chatInput.tsx
+++ components/chat/chatInput.tsx
@@ -4,23 +4,31 @@ import { Textarea } from "@/components/ui/textarea";
 import React, { SetStateAction } from "react";
 
 type Props = {
-  state: string[];
-  setState: React.Dispatch<SetStateAction<string[]>>;
-  ref: { current: string };
+  state: string;
+  setState: React.Dispatch<SetStateAction<string>>;
+  setSendInput: React.Dispatch<SetStateAction<string>>;
   className?: string;
 };
 
-const ChatInput = ({ state, setState, ref, className }: Props) => {
+const ChatInput = ({ state, setState, setSendInput, className }: Props) => {
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setState(e.target.value);
+  };
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    ref.current = formData.get("content") as string;
-    setState((prev) => [ref.current, ...prev]);
-    e.currentTarget.reset();
+    setSendInput(state);
+    setState("");
   };
+
   return (
     <form className={`relative ${className}`} onSubmit={handleSubmit}>
-      <Textarea className="h-full w-full resize-none" placeholder="메시지를 입력하세요." name="content"></Textarea>
+      <Textarea
+        className="h-full w-full resize-none"
+        placeholder="메시지를 입력하세요."
+        value={state}
+        onChange={handleChange}
+      ></Textarea>
       <Button variant="secondary" type="submit" className="absolute bottom-4 right-4">
         작성
       </Button>
