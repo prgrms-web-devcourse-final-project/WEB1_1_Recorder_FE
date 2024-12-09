33a3ca93b1759d1200e027f3c727d8e22ddbccd5
이주환 <wnghks145@gmail.com>
2024-12-09 12:51:41 +0900

    Design : 마이페이지 css 수정

--- app/myPage/editmember/page.tsx
+++ app/myPage/editmember/page.tsx
@@ -25,7 +25,7 @@ const EditMember = ({}: Props) => {
   };
 
   return (
-    <div className="m-auto my-10 h-screen max-w px-4 lg:px-20">
+    <div className="m-auto my-10 h-full max-w px-4 lg:px-20">
       <PageHeader title="마이페이지" />
       <p className="text-24pt mt-10 cursor-pointer border-b-2">회원 정보 수정</p>
       <div className="flex h-screen items-center justify-center">
