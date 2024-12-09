090554b9c8ca68679d249a88f4301ced4bae9fe4
axl0926 <song22092000@gmail.com>
2024-12-09 22:12:07 +0900

    Feat : Header 프로필 불러오도록 수정

--- components/userMenu.tsx
+++ components/userMenu.tsx
@@ -9,30 +9,35 @@ import { useAuthStore } from "@/store/authStore";
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 import Link from "next/link";
+import getMyInfo from "@/services/getMyInfo";
+import { useQuery } from "@tanstack/react-query";
 
 type Props = {};
 
 const UserMenu = ({}: Props) => {
   const { isLogin, checkAuth } = useAuthStore();
   const pathname = usePathname();
+  const { data } = useQuery({ queryKey: ["getMyInfo"], queryFn: getMyInfo });
   useEffect(() => {
     checkAuth();
   }, [checkAuth, pathname]);
   return (
     <>
       {isLogin ? (
-        <div className="flex items-center gap-4">
-          <FaBell size={30} className="cursor-pointer" />
-          <Link href="/chat">
-            <AiFillMessage size={30} className="cursor-pointer" />
-          </Link>
-          <Link className="flex items-center gap-2" href="/myPage">
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png"></AvatarImage>
-            </Avatar>
-            <div>userName</div>
-          </Link>
-        </div>
+        data && (
+          <div className="flex items-center gap-4">
+            <FaBell size={30} className="cursor-pointer" />
+            <Link href="/chat">
+              <AiFillMessage size={30} className="cursor-pointer" />
+            </Link>
+            <Link className="flex items-center gap-2" href="/myPage">
+              <Avatar>
+                <AvatarImage src={data.result.profileImage}></AvatarImage>
+              </Avatar>
+              <div>{data.result.nickname}</div>
+            </Link>
+          </div>
+        )
       ) : (
         <Link href="/login">
           <Button variant="outline">로그인</Button>
